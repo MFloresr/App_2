861156979c1ceabc3a3cf5577f33ca6ad43475c7
MFloresr <mrsitofull@gmail.com>
2014-11-20 14:54:46 +0100

    gg

--- Stack.js
+++ Stack.js
@@ -1,33 +1,30 @@
 function Stack(){
 	this.dataStore = [];
 	this.top = 0;
-
-
 }
 
-Stack.prototype. push(){
+Stack.prototype.push = function(element){
 		this.dataStore[this.top++] = element;
-	}
+}
 
-Stack.prototype.pop(){
-
+Stack.prototype.pop = function(){
 		return	this.dataStore.splice(this.top--,1);
 }
 
-Stack.prototype.peek() {
+Stack.prototype.peek = function() {
 		return this.dataStore[this.top-1];	
 }
 
-Stack.prototype.clear() {
+Stack.prototype.clear = function() {
 	    delete this.dataStore;
 	    this.dataStore = [];
 		this.top = 0;
 }
 
-Stack.prototype.isEmty(){
+Stack.prototype.isEmty = function(){
 	return this.top == 0;
 }
 
-Stack.prototype.length() {
+Stack.prototype.length = function () {
 		return this.top;
 }
---
+++ app_2.js
@@ -0,0 +1,57 @@
+var mistack = new Stack();
+var divContainer = document.getElementById('container');
+
+
+var TextoDeArea = document.createElement('textarea');
+    TextoDeArea.setAttribute('type','textarea');
+    TextoDeArea.setAttribute('id','texto');
+    divContainer.appendChild(TextoDeArea);
+
+var botonComprovar = document.createElement('input');
+	botonComprovar.setAttribute('type','button');
+	botonComprovar.setAttribute('value','Comprovar')
+    divContainer.appendChild(botonComprovar);
+
+var botonClear = document.createElement('input');
+	botonClear.setAttribute('type','button');
+	botonClear.setAttribute('value','Limpiar');
+    divContainer.appendChild(botonClear);
+
+
+function capturarDatos() {
+    var miTexto = document.getElementById('texto').value;
+    for (var i = 0; i < miTexto.length; i++) {
+    	if (miTexto[i] == "("){
+    		mistack.push(i);
+    	}if(miTexto[i] == ")"){
+    		mistack.pop()
+    	}
+
+	}
+	
+	var aux = "";
+	for(var i = (miTexto.length-1); i >= 0; i--){
+		if(i==mistack.peek()){
+			aux ='<span class="vermell">'+miTexto[i]+'</span>'+aux;
+			mistack.pop();
+		}else{
+			aux = miTexto[i]+aux;
+		}
+	}
+
+	var divResultado = document.getElementById('resultado');
+	divResultado.innerHTML= aux;
+
+}
+
+function limpiar() {
+	var divResultado = document.getElementById('resultado');
+	divResultado.innerHTML = "";
+	mistack.clear();
+	
+	var TextoDeArea = document.getElementById('texto');
+	TextoDeArea.value = '';
+}
+
+botonComprovar.addEventListener('click', capturarDatos);
+botonClear.addEventListener('click', limpiar);
